b2535dd01afb6d6e92ef69ee1d01ef659f8b75cd
EXPERTO ITSI <experto@ITSI.EDU>
2026-08-31 16:51:22 -0500

    [ADD] Se creo el formulario login

--- app/Config/Filters.php
+++ app/Config/Filters.php
@@ -34,6 +34,11 @@ class Filters extends BaseFilters
         'forcehttps'    => ForceHTTPS::class,
         'pagecache'     => PageCache::class,
         'performance'   => PerformanceMetrics::class,
+        'csrf'          => \CodeIgniter\Filters\CSRF::class,
+        'toolbar'       => \CodeIgniter\Filters\DebugToolbar::class,
+        'honeypot'      => \CodeIgniter\Filters\Honeypot::class,
+        'auth'          => \App\Filters\AuthFilter::class, // <-- Añadir esta línea
+
     ];
 
     /**
@@ -108,3 +113,6 @@ class Filters extends BaseFilters
      */
     public array $filters = [];
 }
+
+
+
